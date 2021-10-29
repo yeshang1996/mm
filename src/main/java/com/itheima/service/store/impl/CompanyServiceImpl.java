dcdf20cd928d358741d0e65c3ca4aa2e35c03f45
yeshang1996 <979572640@qq.com>
2021-10-29 23:31:21 +0800

    增加业务层接口

---
+++ src/main/java/com/itheima/service/store/impl/CompanyServiceImpl.java
@@ -0,0 +1,39 @@
+package com.itheima.service.store.impl;
+
+import com.github.pagehelper.PageInfo;
+import com.itheima.domain.store.Company;
+import com.itheima.service.store.CompanyService;
+
+import java.util.List;
+
+public class CompanyServiceImpl implements CompanyService {
+    @Override
+    public void save(Company company) {
+
+    }
+
+    @Override
+    public void delete(Company company) {
+
+    }
+
+    @Override
+    public void update(Company company) {
+
+    }
+
+    @Override
+    public Company findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Company> findAll() {
+        return null;
+    }
+
+    @Override
+    public PageInfo findAll(int page, int size) {
+        return null;
+    }
+}
