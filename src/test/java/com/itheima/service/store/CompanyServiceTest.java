7f438cdbb5969e16cdb636701479e7f1891d3ce8
yeshang1996 <979572640@qq.com>
2021-11-04 02:18:10 +0800

    修改测试文件

--- src/test/java/com/itheima/service/store/CompanyServiceTest.java
+++ src/test/java/com/itheima/service/store/CompanyServiceTest.java
@@ -2,16 +2,29 @@ package com.itheima.service.store;
 
 import com.itheima.domain.store.Company;
 import com.itheima.service.store.impl.CompanyServiceImpl;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CompanyServiceTest {
 
+    private  CompanyServiceImpl companyService =null;
+
+    @Before
+    public void init(){
+        CompanyServiceImpl companyService = new CompanyServiceImpl();
+    }
+
     @Test
     public void testSave(){
-        CompanyServiceImpl companyService = new CompanyServiceImpl();
         Company company = new Company();
         company.setName("测试数据1");
         companyService.save(company);
+    }
+
+    @After
+    public void destory(){
+        companyService =null;
     }
 
 }
