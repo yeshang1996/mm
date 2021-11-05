5c2d762b4115e69df0e01d09a055c308e0c7ebb8
yeshang1996 <979572640@qq.com>
2021-11-05 17:50:02 +0800

    添加新增、查询、更新实现

--- src/main/java/com/itheima/service/store/impl/CompanyServiceImpl.java
+++ src/main/java/com/itheima/service/store/impl/CompanyServiceImpl.java
@@ -1,5 +1,6 @@
 package com.itheima.service.store.impl;
 
+import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.itheima.dao.store.CompanyDao;
 import com.itheima.domain.store.Company;
@@ -8,6 +9,7 @@ import com.itheima.service.store.CompanyService;
 import com.itheima.utils.TransactionUtil;
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
@@ -42,26 +44,123 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public void delete(Company company) {
-
+        SqlSession sqlSession=null;
+        try{
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession,CompanyDao.class);
+            //3.调用Dao层操作
+            companyDao.delete(company);
+            //4.提交事务
+            TransactionUtil.commit(sqlSession);
+        }catch (Exception e){
+            TransactionUtil.rollback(sqlSession);
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try{
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void update(Company company) {
-
+        SqlSession sqlSession=null;
+        try{
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession,CompanyDao.class);
+            //3.调用Dao层操作
+            companyDao.update(company);
+            //4.提交事务
+            TransactionUtil.commit(sqlSession);
+        }catch (Exception e){
+            TransactionUtil.rollback(sqlSession);
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try{
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public Company findById(String id) {
-        return null;
+        SqlSession sqlSession=null;
+        try{
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession,CompanyDao.class);
+            //3.调用Dao层操作
+            return companyDao.findById(id);
+        }catch (Exception e){
+            TransactionUtil.rollback(sqlSession);
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try{
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public List<Company> findAll() {
-        return null;
+        SqlSession sqlSession=null;
+        try{
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession,CompanyDao.class);
+            //3.调用Dao层操作
+            return companyDao.findAll();
+        }catch (Exception e){
+            TransactionUtil.rollback(sqlSession);
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try{
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public PageInfo findAll(int page, int size) {
-        return null;
+        SqlSession sqlSession=null;
+        try{
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession,CompanyDao.class);
+            //3.调用Dao层操作
+            PageHelper.startPage(page,size);
+            List<Company> all = companyDao.findAll();
+            PageInfo pageInfo = new PageInfo(all);
+            return  pageInfo;
+        }catch (Exception e){
+            TransactionUtil.rollback(sqlSession);
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try{
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/test/java/com/itheima/service/store/CompanyServiceTest.java
+++ src/test/java/com/itheima/service/store/CompanyServiceTest.java
@@ -1,29 +1,39 @@
 package com.itheima.service.store;
 
+import com.github.pagehelper.PageInfo;
 import com.itheima.domain.store.Company;
 import com.itheima.service.store.impl.CompanyServiceImpl;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.apache.ibatis.ognl.DynamicSubscript;
+import org.junit.*;
+
+import java.util.List;
+
+import static org.apache.ibatis.ognl.DynamicSubscript.all;
 
 public class CompanyServiceTest {
 
-    private  CompanyServiceImpl companyService =null;
+    private static CompanyService companyService =null;
 
-    @Before
-    public void init(){
-        CompanyServiceImpl companyService = new CompanyServiceImpl();
+    @BeforeClass
+    public static void init(){
+        companyService = new CompanyServiceImpl();
     }
 
     @Test
     public void testSave(){
         Company company = new Company();
-        company.setName("测试数据1");
+        company.setName("测试数据5");
         companyService.save(company);
     }
 
-    @After
-    public void destory(){
+    @Test
+    public void testFindAll(){
+        PageInfo all = companyService.findAll(1, 100);
+        System.out.println(all);
+    }
+
+    @AfterClass
+    public static void destroy(){
         companyService =null;
     }
 
