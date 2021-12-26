e5a36ef87503688fb5d5f527236c89d5784636ac
yeshang1996 <979572640@qq.com>
2021-12-27 02:04:09 +0800

    添加新建跳转

--- src/main/java/com/itheima/web/controller/company/CompanyServlet.java
+++ src/main/java/com/itheima/web/controller/company/CompanyServlet.java
@@ -19,28 +19,45 @@ public class CompanyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String operation = request.getParameter("operation");
         if("list".equals(operation)){
-            //进入列表页
-            //获取数据
-            CompanyService companyService = new CompanyServiceImpl();
-            int page = 1;
-            int size = 5;
-            if(StringUtils.isNotBlank(request.getParameter("page"))){
-                page=Integer.parseInt(request.getParameter("page"));
-            }
-            if(StringUtils.isNotBlank(request.getParameter("size"))){
-                size=Integer.parseInt(request.getParameter("size"));
-            }
-            PageInfo all = companyService.findAll(page,size);
-            //将数据保存到指定的位置
-            request.setAttribute("page",all);
-            //跳转页面
-            request.getRequestDispatcher("/WEB-INF/pages/store/company/list.jsp").forward(request,response);
+            this.list(request,response);
         }else if ("toAdd".equals(operation)){
-
+            this.toAdd(request,response);
         }else if ("save".equals(operation)){
+
+        }else if ("list".equals(operation)){
+
+        }else if ("list".equals(operation)){
 
         }
     }
+
+    private void list(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        //进入列表页
+        //获取数据
+        CompanyService companyService = new CompanyServiceImpl();
+        int page = 1;
+        int size = 5;
+        if(StringUtils.isNotBlank(request.getParameter("page"))){
+            page=Integer.parseInt(request.getParameter("page"));
+        }
+        if(StringUtils.isNotBlank(request.getParameter("size"))){
+            size=Integer.parseInt(request.getParameter("size"));
+        }
+        PageInfo all = companyService.findAll(page,size);
+        //将数据保存到指定的位置
+        request.setAttribute("page",all);
+        //跳转页面
+        request.getRequestDispatcher("/WEB-INF/pages/store/company/list.jsp").forward(request,response);
+    }
+
+    private void toAdd(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/pages/store/company/add.jsp").forward(request,response);
+    }
+
+    private void save(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
