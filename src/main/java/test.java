4402cbe3e347b07bbcd1bc242ce91428a2fbeb23
yeshang1996 <979572640@qq.com>
2022-02-11 16:57:53 +0800

    添加了测试类

---
+++ src/main/java/test.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class test {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList();
+        arrayList.add("b");
+        arrayList.add("c");
+        arrayList.add("a");
+        arrayList.add("d");
+        /*Collections.sort(arrayList);
+        System.out.println(arrayList);*/
+
+        /*arrayList.sort(Comparator.reverseOrder());
+        System.out.println(arrayList);*/
+
+        /*arrayList.sort(Comparator.naturalOrder());
+        System.out.println(arrayList);*/
+    }
+}
